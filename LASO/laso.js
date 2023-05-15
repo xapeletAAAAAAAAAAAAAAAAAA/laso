f1e805f44307dcbce13bacf7da5555a012d24a55
natyyp <natypiresoliveira@gmail.com>
2023-05-15 16:43:23 -0300

    laso

---
+++ LASO/laso.js
@@ -0,0 +1,24 @@
+// let i = 0
+
+// while(i < 10){
+//     console.log(i),
+//     // i++
+//     i = i + 1;
+// }
+
+
+// const numeros = [11,12,13,14,15,16,17,22]
+
+// for(let numero of numeros){
+// console.log(numero);
+// }
+
+
+
+// const palavras = ['oi', 'sumido', 'tudo', 'bem?', 'saudades']
+//     let fraseconcatenando = " ";
+//   for(let palavra of palavras){
+//     fraseconcatenando += palavra + " ";
+
+//   }
+// console.log(fraseconcatenando)
